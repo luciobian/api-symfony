1ba788214ef9ee869cf56324e07b772b38ee9f90
luciobian <lbiancardi91@gmail.com>
2019-10-05 17:59:37 -0300

    configuration jwt

---
+++ src/EventSuscriber/PasswordHashSuscriber.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\EventSuscriber;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\KernelEvents;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+
+class PasswordHashSuscriber implements EventSubscriberInterface
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['hashPassword', EventPriorities::PRE_WRITE]
+        ];
+    }
+
+    public function hashPassword(GetResponseForControllerResultEvent $event)
+    {
+        $user = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if (! $user instanceof User || Request::METHOD_POST !== $method){
+            return;
+        }
+
+        $user->setPassword(
+            $this->passwordEncoder->encodePassword($user, $user->getPassword())
+        );
+
+    }
+}
