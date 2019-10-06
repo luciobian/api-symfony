f33038de613888d3803b5ef4b553b32cebc37dad
luciobian <lbiancardi91@gmail.com>
2019-10-05 22:33:41 -0300

    author id from token

---
+++ src/EventSuscriber/AuthoredEntitySuscriber.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\EventSuscriber;
+
+use App\Entity\BlogPost;
+use Symfony\Component\HttpKernel\KernelEvents;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class AuthoredEntitySuscriber implements EventSubscriberInterface
+{
+    /**@var TokenStorageInterface */
+    private $tokenStorage;
+
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['getAuthenticatedUser', EventPriorities::PRE_VALIDATE]
+        ];
+    }
+
+    public function getAuthenticatedUser(GetResponseForControllerResultEvent $event)
+    {
+        $entity = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+        /**@var UserInterface  */
+        $author = $this->tokenStorage->getToken()->getUser();
+
+        if (!$entity instanceof BlogPost || Request::METHOD_POST !== $method) 
+        {
+            return;
+        }
+
+        
+        $entity->setAuthor($author);
+    }
+
+
+}
