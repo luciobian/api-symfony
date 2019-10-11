fc35daf53a4c5fcd145be33a0763f675d83b4601
luciobian <lbiancardi91@gmail.com>
2019-10-10 22:34:50 -0300

    blog post filter

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -1,15 +1,26 @@
 <?php
 namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Annotation\ApiSubresource;
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BlogPostRepository")
+ * @ApiFilter(
+ *      SearchFilter::class,
+ *      properties={
+ *          "id": "exact",
+ *          "title": "partial",
+ *          "content": "partial",
+ *          "author": "exact"
+ *      }
+ * )
  * @ApiResource(
  *     attributes={"order"={"published": "DESC"}}, 
  *     itemOperations={
